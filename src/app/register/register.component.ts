a80383c6044d3da98e4766b0d482297c2444d65b
Ilyass212 <1925619@cegepmontpetit.ca>
2023-09-08 10:03:36 -0400

    FCT Login/Register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,55 +1,32 @@
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
-  baseUrl = "https://localhost:4200/api/";
-  accountBaseUrl = this.baseUrl + "Account/";
-  nom : string | undefined; 
-  password : string | undefined ;
-  passwordConf : string | undefined ;
+  constructor(private http: HttpClient) {}
 
-  hidePassword = true;
-  hideConfirmation = true;
+  register() {
+    const registerData = {
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword,
+    };
 
-  constructor(public http: HttpClient, public cookieService: CookieService) { }
-
-  
-  if(){
-    this.connect()
+    this.http.post('https://localhost:7190/api/Account/Register', registerData, { withCredentials: true }).subscribe(
+      (response: any) => {
+        // L'inscription a réussi, redirigez l'utilisateur vers une page de connexion ou effectuez une autre action appropriée.
+        console.log(response);
+      },
+      (error) => {
+        // Gérez les erreurs d'inscription ici, affichez un message d'erreur, par exemple.
+      }
+    );
   }
-  
-  async connect(){
-    let registerData = {
-      username : this.nom,
-      password : this.password
-    }
-    let result = await lastValueFrom(this.http.post<any>(this.accountBaseUrl + 'Login', registerData));
-    console.log(result);
-  }
-
-  async register(){
-    let registerData = {
-      email : "test@test.com",
-      password : "Passw0rd!",
-      passwordConfrim : "Passw0rd!",
-    }
-    let result = await lastValueFrom(this.http.post<any>(this.accountBaseUrl + 'Register', registerData));
-    console.log(result);
-  }
-
-  
-  ngOnInit() {
-    
-  }
-
 }
